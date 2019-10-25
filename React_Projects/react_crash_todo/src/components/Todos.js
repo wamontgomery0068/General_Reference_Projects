c1e3cd994f3ffcbf65594ba14f5700243abdde3d
Your Name <wamontgomery0068@gmail.com>
2019-10-25 14:01:30 -0600

    Changes made

--- React_Projects/react_crash_todo/src/components/Todos.js
+++ React_Projects/react_crash_todo/src/components/Todos.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+import PropTypes from 'prop-types'
+
 
 class Todos extends Component {
 
   render() {
 
-    console.log(this.props.todos);
+    return this.props.todos.map((todo) => (
 
-    return (
+      <TodoItem key = {todo.id} todo = {todo} />
+     
+    ));
 
-      <div>
-
-       <h1> Todos Component </h1>
-
-      </div>
-
-    );
   }
+}
+
+// PropTypes
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired
 }
 
 export default Todos;
