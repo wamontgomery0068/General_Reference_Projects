c16408725d254da5c5a9093c4e3d95c5ddcf7530
Your Name <wamontgomery0068@gmail.com>
2019-10-25 11:31:46 -0600

    Removed Boilerplate Code

--- React_Projects/react_crash_todo/src/App.js
+++ React_Projects/react_crash_todo/src/App.js
@@ -1,27 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1> Hakuna Matata </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  // Note:
+  // Render is a lifecycle method and it is the only one that is required because it 
+  // needed to render the component in the browser and it is going to return JSX.
+  // In JSX you have to use the className attribute.
+
+  render() {
+
+    return (
+
+      <div className="App">
+
+       
+        
+      </div>
+
+    );
+  }
 }
 
 export default App;
