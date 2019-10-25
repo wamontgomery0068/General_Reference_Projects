cd94c4b59f350f1407debe9e327d8fd6c204077b
Your Name <wamontgomery0068@gmail.com>
2019-10-25 11:41:26 -0600

    Created New Component

---
+++ React_Projects/react_crash_todo/src/components/Todos.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Todos extends Component {
+
+  render() {
+
+    return (
+
+      <div>
+
+       <h1> Todos Component </h1>
+
+      </div>
+
+    );
+  }
+}
+
+export default Todos;
