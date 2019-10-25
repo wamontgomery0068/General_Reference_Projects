e2774112df2cc286bff522a57b700dde4ae7206c
Your Name <wamontgomery0068@gmail.com>
2019-10-25 10:55:05 -0600

    Notes Added

--- React_Projects/react_crash_todo/src/index.js
+++ React_Projects/react_crash_todo/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+// React Dom is rendering the app component into that element with the ID of 'root'
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+
+// Notes
+// The index.js file is the entry point to react
