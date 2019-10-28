34a35ebcf30ed034b9ad9ab15d1dc5006779aef1
Your Name <wamontgomery0068@gmail.com>
2019-10-28 09:00:18 -0600

    Created a new Component

---
+++ React_Projects/react_crash_todo/src/components/AddTodo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class AddTodo extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default AddTodo
