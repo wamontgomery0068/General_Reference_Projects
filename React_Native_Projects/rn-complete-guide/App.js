e4d1e8782aab7e76643610201d6f8b3330d50b34
Your Name <wamontgomery0068@gmail.com>
2019-11-07 10:05:15 -0700

    Incorporated View, TextInput, Button Component

--- React_Native_Projects/rn-complete-guide/App.js
+++ React_Native_Projects/rn-complete-guide/App.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View>
+    <View style = {styles.screen} >
 
-      <View></View>
+      <View 
+        style = {{flexDirection: 'row', justifyContent: 'space-between', alignContent: 'center'}}>
 
-      <View></View>
+        <TextInput 
+          placeholder = "Course Goal" 
+          style = {{width: '80%', borderColor: 'black', borderWidth: 1, padding: 5}} 
+        />
+
+        <Button title = "ADD" />
+
+      </View>
+
+      <View>
+
+      </View>
 
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
+  screen: {
+    padding: 50
+  }
 
 });
