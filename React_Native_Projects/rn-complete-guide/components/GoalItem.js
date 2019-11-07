aca5029fc9f90122868cd3c1da36928862bcd45b
Your Name <wamontgomery0068@gmail.com>
2019-11-07 13:53:15 -0700

    Added the TouchableOpacity Component

--- React_Native_Projects/rn-complete-guide/components/GoalItem.js
+++ React_Native_Projects/rn-complete-guide/components/GoalItem.js
@@ -1,30 +1,24 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 const GoalItem = props => {
-
-    return (
-
-        <View style = {styles.listItem} >
-
-            <Text> {itemData.item.value} </Text> 
-
-        </View>
-    ) 
-    
+  return (
+    <TouchableOpacity onPress = {props.onDelete} >
+      <View style={styles.listItem}>
+        <Text> {props.title} </Text>
+      </View>
+    </TouchableOpacity>
+  );
 };
 
 const styles = StyleSheet.create({
-
-    listItem: {
-        padding: 10,
-        marginVertical: 10,
-        backgroundColor: '#ccc',
-        borderColor: 'black',
-        borderWidth: 1
-    }
-})
-
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1
+  }
+});
 
 export default GoalItem;
